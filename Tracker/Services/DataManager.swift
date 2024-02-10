73b04dafddd79baa973069a0066daa81bd871041
KolAr21 <arina.kolganova21@gmail.com>
2024-02-10 15:45:37 +0700

    Инициализирован NSPersistentContainer

---
+++ Tracker/Services/DataManager.swift
@@ -0,0 +1,26 @@
+//
+//  DataManager.swift
+//  Tracker
+//
+//  Created by Арина Колганова on 10.02.2024.
+//
+
+import Foundation
+import CoreData
+
+final class DataManager {
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Tracker")
+        container.loadPersistentStores { (storeDescription, error) in
+            if let error = error as NSError? {
+                assertionFailure("Не удалось подключить хранилище")
+                return
+            }
+        }
+        return container
+    }()
+
+    lazy var context: NSManagedObjectContext = {
+        persistentContainer.viewContext
+    }()
+}
